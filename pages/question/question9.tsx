758a04cd3e7afbd217378d1386175c75295b679f
김영호 <>
2022-06-13 09:39:24 +0900

    question9

--- pages/question/question9.tsx
+++ pages/question/question9.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 
 const onAdd1 = () => {};
+let n1 = 1;
 const Question9 = () => {
+  let n2 = 2;
   const [count, setCount] = useState(0);
 
   const onAdd2 = () => {};
+
+  console.log("n1", n1);
+  console.log("n2", n2);
   return (
     <>
       <button onClick={() => setCount(count + 1)}>클릭</button>
+      <button
+        onClick={() => {
+          n1 += 1;
+          n2 += 1;
+          setCount(count + 1);
+        }}
+      >
+        클릭
+      </button>
     </>
   );
 };
@@ -15,5 +29,5 @@ const Question9 = () => {
 export default Question9;
 
 // 해답
-// 1. 차이없음
-// 2. 다만 코드의 가독성을 위한 개발자 재량이다.
+// 1. 차이있음
+// 2. 내부 함수는 리렌더링 될 때마다 생성되고 밖의 함수는 처음 구성될 때 한번만 생성됨
