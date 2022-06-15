3fa3b0344ab2fc6a51410cc4065d2ac79011338c
김영호 <>
2022-06-15 11:00:31 +0900

    update directory

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,43 +10,52 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Link href="/">홈</Link>
         </button>
 
-        <button>
-          <Link href="/gugudan">구구단</Link>
-        </button>
+        <li>
+          <button>
+            <Link href="/lecture/gugudan">구구단</Link>
+          </button>
 
-        <button>
-          <Link href="/wordrelay">끝말잇기</Link>
-        </button>
-        <button>
-          <Link href="/question/question1">문제1번</Link>
-        </button>
-        <button>
-          <Link href="/question/question2">문제2번</Link>
-        </button>
-        <button>
-          <Link href="/question/question3">문제3번</Link>
-        </button>
-        <button>
-          <Link href="/question/question4">문제4번</Link>
-        </button>
-        <button>
-          <Link href="/question/question5">문제5번</Link>
-        </button>
-        <button>
-          <Link href="/question/question6">문제6번</Link>
-        </button>
-        <button>
-          <Link href="/question/question7">문제7번</Link>
-        </button>
-        <button>
-          <Link href="/question/question8">문제8번</Link>
-        </button>
-        <button>
-          <Link href="/question/question9">문제9번</Link>
-        </button>
-        <button>
-          <Link href="/useRefSample">useRef</Link>
-        </button>
+          <button>
+            <Link href="/lecture/wordrelay">끝말잇기</Link>
+          </button>
+          <button>
+            <Link href="/lecture/numberBaseball">숫자야구</Link>
+          </button>
+        </li>
+        <li>
+          <button>
+            <Link href="/weekend1/question1">문제1번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question2">문제2번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question3">문제3번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question4">문제4번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question5">문제5번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question6">문제6번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question7">문제7번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question8">문제8번</Link>
+          </button>
+          <button>
+            <Link href="/weekend1/question9">문제9번</Link>
+          </button>
+        </li>
+        <li>
+          <button>
+            <Link href="/study/useRefSample">useRef</Link>
+          </button>
+        </li>
       </ul>
       <Component {...pageProps} />
     </div>
